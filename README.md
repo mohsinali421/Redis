050952129c5f4ba239255fbb18a6e7ecc3ce123e
Mohsin Ali <mohsinali421@gmail.com>
2024-10-17 10:34:34 +0400

    Redis

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
 ReadMe File
-Basic Node api in express for quick setup and do some other stuff
+Basic Redis api in express for quick setup and do some other stuff
 
-Clone and run npm install. Thants it
+# [ Pre Requisite ]
+- docker pull redis
+- docker run --name redis-container -d -p 6379:6379 redis
+
+# Start the project
+- Clone and run npm install.
+- npm start
+
--- app.js
+++ app.js
@@ -1,10 +1,29 @@
 const express = require("express")
 const app = express()
 
+const Redis = require('ioredis');
+const redis = new Redis(); // Connect to localhost:6379
+
+app.use(express.json())
+
 app.get("/", (req,res) => {
     res.json("Hello")
 })
-
+app.post("/:key", async (req,res) => {
+    let { data } = req.body
+    await redis.set('mykey', 'Hello, Redis!')
+    res.json("Set the data")
+})
+app.get("/:key", async (req,res) => {
+    res.json(await redis.get('mykey'))
+})
+app.delete("/:key", async (req,res) => {
+    await redis.del("mykey")
+    res.json("deleted")
+})
 app.listen(3000, () => {
     console.log("Server started at 3000....");
+    console.log("GET http://localhost:3000/mykey");
+    console.log("POST http://localhost:3000/mykey");
+    console.log("DELETE http://localhost:3000/mykey");
 })
